8bbab95dafa33b2e6b935bb403de324c93430736
himanshu <hk7620882@gmail.com>
2023-09-19 20:35:15 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsnIpaFu2YZewiXePW-TbkWJFsbmHa6Us",
+  authDomain: "netfree-9d685.firebaseapp.com",
+  projectId: "netfree-9d685",
+  storageBucket: "netfree-9d685.appspot.com",
+  messagingSenderId: "44227704179",
+  appId: "1:44227704179:web:f38bc821010ad3a39a0106"
+};
